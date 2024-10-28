2042652d33f68a9b5c0b8bf70816345fb19965ef
ALEXGRISHIN1405 <104369794+ALEXGRISHIN1405@users.noreply.github.com>
2024-10-28 21:32:28 +0300

    task2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask # Импорт библиотеки Flask
 
-@app.route('/healthz')
+app = Flask(__name__) # Создание экземпляра приложения
+
+@app.route('/healthz') # Определение маршрута
 def healthz():
   return 'Hello! This is my page for task 2 to INT4 PTStart!!!'
 
-if __name__ == '__main__':
+if __name__ == '__main__': # Запуск приложения
   app.run(debug=True, host='0.0.0.0', port=5000)
